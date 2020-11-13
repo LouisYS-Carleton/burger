4159710201b5b0d7f4000acffbdda2807d5c7762
Louis Yacksmith <louisyscarleton@gmail.com>
2020-11-13 13:29:02 -0500

    fix heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,24 @@
-const mysql = require('mysql2')
+// Load dependencies.
+const mysql = require("mysql2");
 
-const defaultConnection = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'P0pinahat!',
-    database: 'burgers_db'
-};
+let connection;
 
-const connection = mysql.createConnection(defaultConnection);
+// Configure mysql database connection.
+try {
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      port: 3306,
+      password: "P0pinahat!",
+      database: "burgers_db",
+    });
+  }
+} catch (err) {
+  if (err) throw err;
+}
 
+// Export the connection to be used in other files.
 module.exports = connection.promise();
